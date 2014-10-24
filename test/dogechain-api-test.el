12ea4a4c24530de4133154cb7b744d93429a2184
Phil Newton <phil@sodaware.net>
2014-10-24 15:40:09 -0400

    Added test to check endpoint generation with multiple parameters

--- test/dogechain-api-test.el
+++ test/dogechain-api-test.el
@@ -3,3 +3,9 @@
 (ert-deftest dogechain-api-test/can-create-simple-endpoint-without-params ()
   (should (string= "http://dogechain.info/chain/Dogecoin/q/testmethod/"
                    (dogechain-api--build-simple-endpoint "testmethod"))))
+
+(ert-deftest dogechain-api-test/can-create-simple-endpoint-with-params ()
+  (should (string= "http://dogechain.info/chain/Dogecoin/q/testmethod/param-1"
+                   (dogechain-api--build-simple-endpoint "testmethod" "param-1")))
+  (should (string= "http://dogechain.info/chain/Dogecoin/q/testmethod/param-1/param-2"
+                   (dogechain-api--build-simple-endpoint "testmethod" "param-1" "param-2"))))
